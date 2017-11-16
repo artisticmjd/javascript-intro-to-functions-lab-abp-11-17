ea2db83af2092f5790bdee3959f0239ce7ac9918
Michael Delgadillo <protectordelafe@yahoo.com>
2017-11-16 05:50:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  let whisperHi = 'hello'
-  if (whisperHi.toLowerCase === whisperHi){
-      console.log("I can't hear you!")
+  let whisperHi = string.toLowerCase
+  if(whisperHi.toLowerCase === whisperHi){
+    console.log("I can't hear you!")
   }
 }
