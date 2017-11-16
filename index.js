47000365beac2cd99a9367ffa36016d1a43af641
Michael Delgadillo <protectordelafe@yahoo.com>
2017-11-16 06:21:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ function whisper(string){
 }
 
 function logShout(string){
-  //let shout = string.toUpperCase()
   console.log(string.toUpperCase())
 }
 
 function logWhisper(string){
-  let whisper = string.toLowerCase()
-  console.log(whisper)
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string){
